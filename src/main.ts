92ce8c45b591a68c55a2ca9abf804ae6f1e67ffa
“Yasith <48174322+yasithKumara@users.noreply.github.com>
2021-04-28 16:21:42 +0530

    first commit

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,8 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
+  import { defineCustomElements } from '@ionic/pwa-elements/loader';
+
+  // Call the element loader after the platform has been bootstrapped
+  defineCustomElements(window);
